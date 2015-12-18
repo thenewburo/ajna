fc68e30ab98d16b735d7d393fe5d8d8f43102e5f
Julien Regnier <julien.regnier10@gmail.com>
2015-12-18 02:40:34 +0100

    Add the first 3 pages of the app + the login page + the translate module

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TheNewBuro_Kroot
 Kroot's project by TheNewBuro
+
+
+
+Prerequisites:
+
+- NodeJS
+- Cordova (use "npm install -g cordova" to install)
+- Ionic (use "npm install -g ionic" to install)
+- Gulp (use "npm install -g gulp" to install)
+
+
+
+How to run in your computer browser:
+
+In the project root folder:  ionic serve
+
+
+
+How to run in another platform:
+
+In the project root folder, check the available platforms:  ionic platform list
+Install the desired platform if needed:  ionic platform add <platform>
+Build the project:  ionic build <platform>
+Run the project:  ionic run <platform>
+(it will automatically find a device like an emulator or a connected phone)
--- www/js/app.js
+++ www/js/app.js
@@ -4,70 +4,105 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'pascalprecht.translate'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.hide();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function($stateProvider, $urlRouterProvider, $translateProvider) {
+    $stateProvider
 
+    .state('onBoarding1', {
+        url: '/onBoarding1',
+        templateUrl: 'templates/onBoardingPage1.html',
+        controller: 'OnBoardingCtrl'
+    })
+
+    .state('onBoarding2', {
+        url: '/onBoarding2',
+        templateUrl: 'templates/onBoardingPage2.html',
+        controller: 'OnBoardingCtrl'
+    })
+
+    .state('onBoarding3', {
+        url: '/onBoarding3',
+        templateUrl: 'templates/onBoardingPage3.html',
+        controller: 'OnBoardingCtrl'
+    })
+
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    });
+/*
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+    .state('app.search', {
+        url: '/search',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/search.html'
+            }
+        }
+    })
+
+    .state('app.browse', {
+            url: '/browse',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/browse.html'
+                }
+            }
+        })
+        .state('app.playlists', {
+            url: '/playlists',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlists.html',
+                    controller: 'PlaylistsCtrl'
+                }
+            }
+        })
+
+    .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/playlist.html',
+                controller: 'PlaylistCtrl'
+            }
+        }
+    });*/
+
+    // Set the path to the translation files
+    $translateProvider.useStaticFilesLoader({
+        prefix: '../lng/locale-',
+        suffix: '.json'
+    });
+
+    // English by default
+    $translateProvider.useSanitizeValueStrategy('escaped');
+    $translateProvider.preferredLanguage('EN');
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/onBoarding1');
 });
