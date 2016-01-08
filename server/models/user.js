6c97f15ead634a3c8694eef800b4cb903733c456
Julien Regnier <julien.regnier10@gmail.com>
2016-01-08 02:40:16 +0100

    Fix a bug in server.js

--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
 	email: { type: String, required: true, unique: true },
-	name: { type: String, required: true},
-	password: { type: String, required: true},
+	name: { type: String, required: true },
+	password: { type: String },
 	decks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Deck' }],
 	ownedDecks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Store' }],
 	facebook: {
